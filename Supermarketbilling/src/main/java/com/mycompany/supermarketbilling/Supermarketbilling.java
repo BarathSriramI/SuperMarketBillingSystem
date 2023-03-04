ef8e4c006fb060d00a1036848a17b15ee70e6c1c
BarathSriramI <125194795+BarathSriramI@users.noreply.github.com>
2023-03-04 12:52:04 +0530

    Add files via upload

---
+++ Supermarketbilling/src/main/java/com/mycompany/supermarketbilling/Supermarketbilling.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.supermarketbilling;
+
+/**
+ *
+ * @author DELL
+ */
+public class Supermarketbilling {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
